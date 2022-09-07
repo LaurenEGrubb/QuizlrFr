a18f8e92a8116370801779f62e823dd3c37f0fb7
laurengrubb <lauren.elyse.grubb@gmail.com>
2022-09-06 20:39:12 -0400

    imported pages to the app.js

--- quizlr/src/App.js
+++ quizlr/src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import { Nav } from './components/Nav'
+import FlashCard from './components/FlashCard'
+import Study from './pages/Study';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    
       </header>
     </div>
   );
