a972695db478d7208cee82a4510e27712fb84369
laurengrubb <lauren.elyse.grubb@gmail.com>
2022-09-13 10:40:08 -0400

    created post services

--- quizlr/src/pages/Study.js
+++ quizlr/src/pages/Study.js
@@ -4,6 +4,23 @@ function Study (){
     return (
       <div>
          <h4>Study</h4>
+         <div>
+        {flashcard?.map((flashcard) => (
+          <div className="flash-card" key={flashcard.id}>
+            <h3>{flashcard.term}</h3>
+            <p>{flashcard.definition}</p>
+
+    
+            <Link className="editbutton"to={`/edit/${flashcard.id}`}>Edit Flashcard</Link>
+            {/* <EditPost posts={posts} /> */}
+
+            <div>
+              <button onClick={() => deleteFlashcard(flashcard.id)} className="delete">X</button>
+              <div></div>
+            </div>
+          </div>
+        ))}
+      </div>
           
       </div>
   
